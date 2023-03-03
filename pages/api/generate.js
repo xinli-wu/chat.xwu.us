3aa6d477a480fbbef3d76da2efe94707cf1c0b73
Xinli WU <ryan7gm@gmail.com>
2023-03-03 22:45:12 +1100

    init

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -32,7 +32,7 @@ export default async function (req, res) {
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
