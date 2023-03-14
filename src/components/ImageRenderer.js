e40ba186a4b58dbbbcbe0ad865ef01d7ff0c1c57
Xinli WU <ryan7gm@gmail.com>
2023-03-15 00:17:05 +1100

    css

--- src/components/ImageRenderer.js
+++ src/components/ImageRenderer.js
@@ -19,6 +19,7 @@ export const ImageRenderer = ({ b64_json }) => {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '70vw',
+    height: '70vh',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
