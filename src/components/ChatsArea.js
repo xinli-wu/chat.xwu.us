a2b597ae89ed7ed5b201e8df57f34781f467b808
Xinli WU <ryan7gm@gmail.com>
2023-04-11 04:48:35 +1000

    test

--- src/components/ChatsArea.js
+++ src/components/ChatsArea.js
@@ -12,7 +12,7 @@ export const ChatsArea = ({ children }) => {
       justifyContent: 'space-between',
       alignItems: 'center',
       flexFlow: 'column nowrap',
-      height: 'calc(100vh - 80px)'
+      height: 'calc(100vh - 80px - env(safe-area-inset-bottom))'
     }}>
       {children}
     </Box>
